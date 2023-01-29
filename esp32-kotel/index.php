57a9b621af0717309df42ff6ebc79970a88e1854
Adam Juraszek <adam.juraszek@ataccama.com>
2023-01-29 10:04:28 +0100

    Esp to influx

--- esp32-kotel/index.php
+++ esp32-kotel/index.php
@@ -1,9 +1,46 @@
 <?php
 
-$b = $_GET['boot'];
+file_put_contents('update', json_encode([$_GET]));
 
-echo ($b * 2);
 
-$line = date(DATE_ISO8601) . " " . $b . "\n";
+function toInflux()
+{
+    $boot_cnt = (int)$_GET['boot'];
+    $rssi_dbm = (int)$_GET['rssi'];
+    $temp1_c = (float)$_GET['temp1'];
+    $temp2_c = (float)$_GET['temp2'];
+    $temp3_c = (float)$_GET['temp3'];
+    $light1_lux = (int)$_GET['light1'];
 
-file_put_contents("log", $line, FILE_APPEND);
+    $content = [
+        'boot_cnt' => $boot_cnt,
+        'rssi_dbm' => $rssi_dbm,
+        'temp1_c' => $temp1_c,
+        'temp2_c' => $temp2_c,
+        'temp3_c' => $temp3_c,
+        'light1_lux' => $light1_lux
+    ];
+
+    $opts = [
+        'http' => [
+            'method' => "POST",
+            'content' => "esp32-kotel " . http_build_query($content, '', ','),
+        ]
+    ];
+
+    $params = [
+        "db" => "basement"
+    ];
+
+    file_put_contents('influx', json_encode($opts));
+
+    file_get_contents(
+        'http://localhost:18086/write?' . http_build_query($params),
+        false,
+        stream_context_create($opts)
+    );
+}
+
+toInflux();
+
+echo "OK";
